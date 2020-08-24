f582170494831be27e1feb4fb68416bdd30c3d5b
AFJohan92 <johan.sebastian.bonilla@cern.ch>
2020-08-24 06:52:59 -0500

    Adding skeleton class CSCStubEfficiencyValidation, accordingly included in CSCDigiValidation

--- Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
+++ Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
@@ -1,4 +1,7 @@
 import FWCore.ParameterSet.Config as cms
+from Validation.MuonHits.muonSimHitMatcherPSet import *
+from Validation.MuonCSCDigis.muonCSCStubPSet import *
+from Validation.MuonCSCDigis.muonCSCDigiPSet import *
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 cscDigiValidation = DQMEDAnalyzer('CSCDigiValidation',
@@ -9,7 +12,11 @@ cscDigiValidation = DQMEDAnalyzer('CSCDigiValidation',
     comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
     alctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
     clctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
-    doSim = cms.bool(False)
+    doSim = cms.bool(False),
+    #muonSimHitMatcherPSet,
+    #muonCSCStubPSet,
+    #muonCSCDigiPSet
+    #And GEM matcher
 )
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
