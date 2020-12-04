0f02407fce81f72b1c7a7ebbcbf02e02be534e57
Sven Dildick <sven.dildick@cern.ch>
2020-12-04 13:50:49 -0600

    Working validation and harvesting steps

--- Validation/MuonCSCDigis/test/runCSCDigiHarvesting_cfg.py
+++ Validation/MuonCSCDigis/test/runCSCDigiHarvesting_cfg.py
@@ -10,52 +10,34 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load("Validation.MuonCSCDigis.cscDigiValidation_cfi")
 process.load('Configuration.StandardSequences.DQMSaverAtRunEnd_cff')
+process.load('Configuration.StandardSequences.Harvesting_cff')
 process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10),
+    input = cms.untracked.int32(-1),
     output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
 )
 
 # Input source
-process.source = cms.Source(
-    "PoolSource",
-    fileNames = cms.untracked.vstring(
-        "/store/relval/CMSSW_11_2_0_pre7/RelValSingleMuPt10/GEN-SIM-DIGI-RAW/112X_mcRun3_2021_realistic_v8-v1/20000/0ED98457-2CEC-924D-AAFC-4F3F705C2DCC.root"
-    ),
-    secondaryFileNames = cms.untracked.vstring()
+process.source = cms.Source("DQMRootSource",
+    fileNames = cms.untracked.vstring('file:step3_inDQM.root')
 )
 
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2021_realistic', '')
 
-process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('DQMIO'),
-        filterName = cms.untracked.string('')
-    ),
-    fileName = cms.untracked.string('file:step3_inDQM.root'),
-    outputCommands = process.DQMEventContent.outputCommands,
-    splitLevel = cms.untracked.int32(0)
-)
-
 # Path and EndPath definitions
-process.validation_step = cms.Path(process.mix * process.cscDigiValidation)
-#process.dqmsave_step = cms.Path(process.DQMSaver)
+process.harvesting_step = cms.Path(process.cscDigiHarvesting)
+process.dqmsave_step = cms.Path(process.DQMSaver)
 process.endjob_step = cms.EndPath(process.endOfProcess)
-process.DQMoutput_step = cms.EndPath(process.DQMoutput)
 
 # Schedule definition
 process.schedule = cms.Schedule(
-    process.validation_step,
+    process.harvesting_step,
     process.endjob_step,
-    process.DQMoutput_step
-    #process.dqmsave_step
+    process.dqmsave_step
 )
